45ff16ac0866f7a1f1a9b3a4539bb8c2a9bcbf34
Technology for Social Impact <58343309+tsiAILAB@users.noreply.github.com>
2020-03-14 15:23:41 +0600

    JOB Files & Intel DEV Cloud Edge Inference output
    JOB Files & Intel DEV Cloud Edge Inference output

---
+++ maize_job.sh
@@ -0,0 +1,3 @@
+
+cd /home/u37452/Model_32/EdgeComputation
+python3 tsiai_devcloud_inference.py  -m m_maize_tensorflowmodel.xml -i MHL.JPG                                                                                                                                                                                                                                                                  
---
+++ potato_job.sh
@@ -0,0 +1,3 @@
+
+cd /home/u37452/Model_32/EdgeComputation
+python3 tsiai_devcloud_inference.py  -m m_potato_tensorflowmodel.xml -i PHL.JPG                                                                                                                                                                                                                                                                  
