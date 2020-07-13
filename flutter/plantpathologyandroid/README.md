9ed869e9a5f227e60b5a758692946a131025f637
firozahmedtsi <firoz.ahmed@tsi.com.bd>
2020-07-13 19:33:04 +0600

    Signed-off-by: firozahmedtsi <firoz.ahmed@tsi.com.bd>

--- flutter/plantpathologyandroid/README.md
+++
@@ -1,5 +0,0 @@
-# FlutterApp: Get Image and send to Server
-
-Let's start with Flutter Framework with Demo App
-
-FlutterProjectSetup.docx(Flutter Environment Setup and Complete Project Guide Line)
--- web-app/web-app/controllers/logout_controller.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-unset($_SESSION['IS_LOGIN_ADMIN']);
-unset($_SESSION['IS_LOGIN_USER']);
-unset($_SESSION['EMAIL']);
-header('Location:../views/authentications/login_page.php');
-die();
-?>
